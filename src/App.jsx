34cc742d7310fbc74583a91a4a0178cb9f13b5a4
shazab saifi <shazebsaifi92@gmail.com>
2024-07-26 20:52:28 +0530

    Authentication completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>This is a project</h1>
+    </>
+  )
+}
+
+export default App
