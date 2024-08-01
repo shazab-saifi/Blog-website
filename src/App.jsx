8a5112db42b278fbd453bb2ee0c9e72e7d135cd6
shazab saifi <shazebsaifi92@gmail.com>
2024-08-01 19:48:41 +0530

    Just modified few files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import './App.css'
 import authService from './appwrite/auth'
 import { login, logout } from './store/authSlice'
 import Header from './components/Header/Header'
