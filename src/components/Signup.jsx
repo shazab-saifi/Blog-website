678a17e7972c97a128aee2b934b754c87f7c5127
shazab saifi <shazebsaifi92@gmail.com>
2024-08-05 20:47:11 +0530

    there was a bug in Signup,jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import authService from '../appwrite/auth'
 import { Link, useNavigate } from 'react-router-dom'
 import { login } from '../store/authSlice'
